2caa03c13d9869eeb619152c29d22b44ebe06319
sonots <sonots@8ef9a475-ed55-0410-b63f-f98aa18d2370>
2008-10-07 04:37:58 +0000

    separate files

---
+++ trunk/convert_format.h
@@ -0,0 +1,99 @@
+/**
+* The MIT License
+* 
+* Copyright (c) 2008, Naotoshi Seo <sonots(at)sonots.com>
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+#include <stdio.h>
+#include <string>
+
+#ifndef CONVERT_FORMAT_INCLUDED
+#define CONVERT_FORMAT_INCLUDED
+
+/**
+* Convert format
+*
+* %i => filename
+* %e => extension
+* %x => x
+* %y => y
+* %w => width
+* %h => height
+* %r => rotation (radian * 1000)
+* %f => frame number (for video file)
+*
+* @param format The format string
+* @return string
+* @todo refine more (use boost::any or use boost::regex)
+*/
+string convert_format( const string& format, const string& dirname, const string& filename, const string& extension, 
+                      int x, int y, int width, int height, int frame = 0, int rotation = 0 )
+{
+    string ret = format;
+    char tmp[2048];
+    char intkeys[] = { 'x', 'y', 'w', 'h', 'f', 'r' };
+    int  intvals[] = { x, y, width, height, frame, rotation };
+    char strkeys[] = { 'i', 'e', 'd' };
+    std::string strvals[] = { filename, extension, dirname };
+    int nintkeys = 6;
+    int nstrkeys = 3;
+    for( int i = 0; i < nintkeys + nstrkeys; i++ )
+    {
+        std::string::size_type start = ret.find( "%" );
+        if( start == std::string::npos ) break;
+        std::string::size_type minstrpos = std::string::npos;
+        std::string::size_type minintpos = std::string::npos;
+        int minstrkey = INT_MAX; int minintkey = INT_MAX;
+        for( int j = 0; j < nstrkeys; j++ )
+        {
+            std::string::size_type pos = ret.find( strkeys[j], start );
+            if( pos < minstrpos )
+            {
+                minstrpos = pos;
+                minstrkey = j;
+            }
+        }
+        for( int j = 0; j < nintkeys; j++ )
+        {
+            std::string::size_type pos = ret.find( intkeys[j], start );
+            if( pos < minintpos )
+            {
+                minintpos = pos;
+                minintkey = j;
+            }
+        }
+        if( minstrpos == std::string::npos && minintpos == std::string::npos ) break;
+        if( minstrpos < minintpos )
+        {
+            string format_substr = ret.substr( start, minstrpos - start ) + "s";
+            sprintf( tmp, format_substr.c_str(), strvals[minstrkey].c_str() );
+            ret.replace( start, minstrpos - start + 1, string( tmp ) );
+        }
+        else
+        {
+            string format_substr = ret.substr( start, minintpos - start ) + "d";
+            sprintf( tmp, format_substr.c_str(), intvals[minintkey] );
+            ret.replace( start, minintpos - start + 1, string( tmp ) );
+        }
+    }
+    return ret;
+}
+
+#endif
---
+++ trunk/get_filelist.h
@@ -0,0 +1,90 @@
+/**
+* The MIT License
+* 
+* Copyright (c) 2008, Naotoshi Seo <sonots(at)sonots.com>
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+#include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
+#include <vector>
+using namespace std;
+namespace fs = boost::filesystem;
+
+#ifndef GET_FILELIST_INCLUDED
+#define GET_FILELIST_INCLUDED
+/**
+* List Files in a directory
+*
+* @param dirpath Path to the target directory
+* @param [regex = ".*"] Regular expression (instead of wild cards)
+* @param [file_type = type_unknown] List only specified file_type. The default is for all
+* @return vector<boost::filesystem::path> Vector of file list
+* @requirements boost::filesystem, boost::regex, std::vector, std::string 
+*
+* Memo: boost::filesystem::path path.native_file_string().c_str()
+*/
+vector<fs::path> get_filelist( const fs::path& dirpath, 
+                              const boost::regex regex = boost::regex(".*"), 
+                              fs::file_type file_type = fs::type_unknown )
+{
+    vector<fs::path> filelist;
+    bool list_directory    = ( file_type == fs::directory_file );
+    bool list_regular_file = ( file_type == fs::regular_file );
+    bool list_symlink      = ( file_type == fs::symlink_file );
+    bool list_other        = ( !list_directory && !list_regular_file && !list_symlink );
+    bool list_all          = ( file_type == fs::type_unknown ); // just for now
+
+    if( !fs::exists( dirpath ) || !fs::is_directory( dirpath ) )
+    {
+        return filelist;
+    }
+
+    fs::directory_iterator iter( dirpath ), end_iter;
+    for( ; iter != end_iter; ++iter )
+    {
+        fs::path filename = iter->path();
+        if( boost::regex_match( filename.native_file_string(), regex ) )
+        {
+            if( list_all )
+            {
+                filelist.push_back( filename );
+            }
+            else if( list_regular_file && fs::is_regular( filename ) )
+            {
+                filelist.push_back( filename );                
+            }
+            else if( list_directory && fs::is_directory( filename ) )
+            {
+                filelist.push_back( filename );
+            }
+            else if( list_symlink && fs::is_symlink( filename ) )
+            {
+                filelist.push_back( filename );
+            }
+            else if( list_other && fs::is_other( filename ) )
+            {
+                filelist.push_back( filename );
+            }
+        }
+    }
+    return filelist;
+}
+
+#endif
